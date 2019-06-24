84579379b744839feda45a3b246748b2860ebdf3
Михаил Колотилин <kolotilinma@gmail.com>
2019-06-24 23:00:43 +0300

    Custom Table view Cell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Михаил on 24/06/2019.
+//  Copyright © 2019 Михаил. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var NameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+
+}
