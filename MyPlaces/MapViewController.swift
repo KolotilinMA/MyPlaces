e7c46e287989cfd47d3932d32c434d9fb7950945
Михаил Колотилин <kolotilinma@gmail.com>
2019-06-29 12:22:01 +0300

    Подготовка MapViewController

---
+++ MyPlaces/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  MyPlaces
+//
+//  Created by Михаил on 29/06/2019.
+//  Copyright © 2019 Михаил. All rights reserved.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    @IBAction func closeVC() {
+        dismiss(animated: true, completion: nil)
+    }
+}
